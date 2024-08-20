d4b04528cd14006f5874b751d49b2db203a146f2
IvanTsers <ivantsres@gmail.com>
2024-08-20 11:28:40 +0200

    minor updates 	replaced a call to the custom MinAncLen() with sus.Quantile() 	added multiple queries support and Intersect() 	swapped -s and -r flag names 	added a commented-out replace for importing the local ancs instead of the remote

--- alan.go
+++ alan.go
@@ -6,38 +6,50 @@ import (
 	"github.com/evolbioinf/clio"
 	//"github.com/ivantsers/alan/util"
 	"github.com/evolbioinf/esa"
+	"github.com/evolbioinf/sus"
 	"github.com/ivantsers/ancs"
 	"github.com/ivantsers/fasta"
 	"os"
 )
 
 func main() {
-	var optS = flag.String("s", "", "subject sequence")
-	var optQ = flag.String("q", "", "query sequence")
+	var optR = flag.String("r", "", "reference sequence")
 	var optA = flag.Int("a", 0, "minimum anchor length")
-	var optP = flag.Float64("p", 0.025,
+	var optP = flag.Float64("p", 0.05,
 		"p-value for a non-random shustring")
 	var optVerb = flag.Bool("verbose", false, "toggle verbose mode")
 	var optN = flag.Bool("n", false, "print segregation sites (Ns)"+
 		"in the output sequences")
-	var optR = flag.Bool("r", false, "print segregation site ranges"+
+	var optS = flag.Bool("s", false, "print segregation site ranges"+
 		"to stderr")
+	var optF = flag.Float64("f", 1.0, "intersection sensitivity threshold")
 	var optV = flag.Bool("v", false, "print version and "+
 		"program information")
 	u := "alan [option]..."
-	p := "Find approximate local alignment of two sequences"
+	p := "Find approximate local alignment"
 	e := "alan -s subject.fasta -q query.fasta"
 	clio.Usage(u, p, e)
 	flag.Parse()
 	if *optV {
-		fmt.Println("alan v0.1")
+		fmt.Println("alan v0.2")
 	}
-	if !(*optQ != "" && *optS != "") {
-		fmt.Fprintf(os.Stderr, "please give names "+
-			"of subject and query files.\n")
+	fileNames := flag.Args()
+	if len(fileNames) < 2 {
+		fmt.Fprintf(os.Stderr, "please specify at least two input files\n")
 		os.Exit(1)
 	}
-	f, _ := os.Open(*optS)
+	queryNames := []string{}
+	if *optR == "" {
+		fmt.Fprintf(os.Stderr, "please specify the reference sequence\n")
+		os.Exit(1)
+	} else {
+		for _, fileName := range fileNames {
+			if fileName != *optR {
+				queryNames = append(queryNames, fileName)
+			}
+		}
+	}
+	f, _ := os.Open(*optR)
 	subjectContigs := fasta.ReadAll(f)
 	f.Close()
 	subject := fasta.Concatenate(subjectContigs, 0)
@@ -52,32 +64,42 @@ func main() {
 	if *optA > 0 {
 		sma = *optA
 	} else {
-		sma = ancs.MinAncLen(sl/2, sgc, 1.0-*optP)
+		sma = sus.Quantile(sl/2, sgc, 1.0-*optP)
 	}
 	if *optVerb {
 		fmt.Fprintf(os.Stderr,
 			"# minimum anchor length: %d\n", sma)
 	}
-	f, _ = os.Open(*optQ)
-	queryContigs := fasta.ReadAll(f)
-	f.Close()
-	query := fasta.Concatenate(queryContigs, 0)
-	query.Clean()
-	homologies, segsites := ancs.FindHomologies(query, se, sl, sma)
-	ancs.SortByStart(homologies)
-	homologies = ancs.ReduceOverlaps(homologies)
+	numQueries := len(queryNames)
+	allH := []ancs.Seg{}
+	allNs := make(map[int]bool)
+	for _, q := range queryNames {
+		f, _ = os.Open(q)
+		queryContigs := fasta.ReadAll(f)
+		f.Close()
+		query := fasta.Concatenate(queryContigs, 0)
+		query.Clean()
+		h, ns := ancs.FindHomologies(query, subject, se, sma)
+		ancs.SortByStart(h)
+		allH = append(allH, ancs.ReduceOverlaps(h)...)
+		for pos, _ := range ns {
+			allNs[pos] = true
+		}
+	}
+	sensitivity := *optF
+	intersection := ancs.Intersect(allH, numQueries, sensitivity, sl/2)
 	if *optVerb {
 		fmt.Fprintf(os.Stderr,
-			"# %d homologous segments(s)\n", len(homologies))
+			"# %d homologous segments(s)\n", len(intersection))
 		fmt.Fprintf(os.Stderr,
-			"# total length: %d\n", ancs.TotalSegLen(homologies))
+			"# total length: %d\n", ancs.TotalSegLen(intersection))
 		fmt.Fprintf(os.Stderr,
-			"# %d segregation site(s)\n", len(segsites))
+			"# %d segregation site(s)\n", len(allNs))
 	}
-	if *optR {
-		ancs.PrintSegsiteRanges(segsites, os.Stderr)
+	if *optS {
+		ancs.PrintSegsiteRanges(allNs, intersection, os.Stderr)
 	}
-	result := ancs.SegToFasta(homologies, se, segsites, *optN)
+	result := ancs.SegToFasta(intersection, se, allNs, *optN)
 	for _, seq := range result {
 		fmt.Fprintf(os.Stdout, "%s\n", seq)
 	}
--- go.mod
+++ go.mod
@@ -1,14 +1,13 @@
 module alan
 
-go 1.22.5
+go 1.22.2
 
 require (
 	github.com/evolbioinf/clio v0.0.0-20230620105705-02d07225d27e
 	github.com/evolbioinf/esa v0.0.0-20240208112648-445905ef2b6d
+	github.com/evolbioinf/sus v0.0.0-20230320163303-b6d16dd4ec1f
+	github.com/ivantsers/ancs v0.0.0-20240813101225-9d517c1da0b0
 	github.com/ivantsers/fasta v0.0.0-20240716130655-4086cb635901
 )
 
-require (
-	github.com/evolbioinf/sus v0.0.0-20230320163303-b6d16dd4ec1f // indirect
-	github.com/ivantsers/ancs v0.0.0-20240717090412-ee7328f05409 // indirect
-)
+//replace github.com/ivantsers/ancs => ../ancs
