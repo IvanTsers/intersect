ba97e44ecfa9f462b01046d90c2aa01a0c51d96b
IvanTsers <ivantsres@gmail.com>
2024-08-31 10:12:51 +0200

    fixed flags, docs, added Readme

--- intersect.go
+++ intersect.go
@@ -7,24 +7,26 @@ import (
 	"github.com/ivantsers/chr"
 	"github.com/ivantsers/fasta"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
 	var optR = flag.String("r", "", "reference sequence")
 	var optD = flag.String("d", "", "directory of target sequences")
-	optP := flag.Float64("p", 0.05,
-		"p-value for a non-random matching shustring length")
+	optP := flag.Float64("p", 0.95,
+		"threshold p-value for a shustring length")
 	optVerb := flag.Bool("verbose", false, "toggle verbose mode")
 	optN := flag.Bool("n", false, "print segregation sites (Ns)"+
 		"in the output sequences")
 	optS := flag.Bool("s", false, "print segregation site ranges"+
 		"in the headers")
 	optF := flag.Float64("f", 1.0, "intersection sensitivity threshold")
-	optCleanR := flag.Bool("clean-reference", true,
+	optCleanR := flag.Bool("clean-reference", false,
 		"remove non-ATGC nucleotides from the reference")
-	optCleanQ := flag.Bool("clean-queries", true,
+	optCleanQ := flag.Bool("clean-queries", false,
 		"remove non-ATGC nucleotides from the queries")
-	optOneBased := flag.Bool("one-based-output", true,
+	optOneBased := flag.Bool("one-based-output", false,
 		"print one-based end-exclusive coordinates in the "+
 			"output headers. The default coordinates "+
 			"are zero-based end-inclusive.")
@@ -57,7 +59,6 @@ func main() {
 			"can't use %v as a sensitivity threshold\n", *optF)
 		os.Exit(1)
 	}
-	pval := 1.0 - *optP
 	if *optF > 1.0 || *optF <= 0.0 {
 		fmt.Fprintf(os.Stderr,
 			"can't use %v as a sensitivity threshold\n", *optF)
@@ -67,7 +68,7 @@ func main() {
 		Reference:       referenceContigs,
 		TargetDir:       *optD,
 		Threshold:       *optF,
-		ShustrPval:      pval,
+		ShustrPval:      *optP,
 		CleanSubject:    *optCleanR,
 		CleanQuery:      *optCleanQ,
 		PrintSegSitePos: *optS,
@@ -77,15 +78,28 @@ func main() {
 	isc := chr.Intersect(parameters)
 	if *optVerb {
 		totalLen := 0
+		numNs := 0
 		for _, seq := range isc {
 			totalLen += seq.Length()
+			if *optS {
+				r, _ := regexp.Compile(` [0-9]+;`)
+				match := r.FindString(seq.Header())
+				matchBytes := []byte(match)
+				num, _ := strconv.Atoi(string(matchBytes[1 : len(match)-1]))
+				numNs += num
+			}
 		}
 		fmt.Fprintf(os.Stderr, "# Intersected sequences from %d"+
-			" n files\n", numFiles+1)
+			" files\n", numFiles+1)
 		fmt.Fprintf(os.Stderr,
 			"#  common homologous region(s): %d\n", len(isc))
 		fmt.Fprintf(os.Stderr,
 			"#  intersection's total length: %d\n", totalLen)
+		if *optS {
+			fmt.Fprintf(os.Stderr,
+				"#  number of segregation sites: %d\n", numNs)
+		}
+
 	}
 	for _, seq := range isc {
 		fmt.Fprintf(os.Stdout, "%s\n", seq)
